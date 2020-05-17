9aca47d458adcc99b8f43b6a60e44d63f89f9b37
ridhianand16 <ridhianand16@gmail.com>
2020-05-18 01:39:50 +0530

    Adding Basic UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,48 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.blueGrey,
         appBar: AppBar(
-          title: Center(
-            child: Text('Pothole and Crack Reporting')
-          ),
+          title: Center(child: Text('Pothole and Crack Reporting')),
           backgroundColor: Colors.blueGrey[900],
         ),
-        body: Center(
-          child: Text ("Take a photo and upload"),
-        )
+        body: MyBody(),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () => {},
+          tooltip: 'Increment',
+          child: Icon(Icons.add),
+        ),
       ),
-    ),
-  );
+    );
+  }
+}
+
+class MyBody extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: <Widget>[
+          Padding(
+              padding: const EdgeInsets.all(50.0),
+              child: Text('Location - '),
+            ),
+
+
+          new RaisedButton(
+            onPressed: () {},
+            child: Text('Report Issue'),
+          ),
+        ],
+      ),
+    );
+  }
 }
